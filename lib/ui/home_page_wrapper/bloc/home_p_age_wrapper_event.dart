a8886a339aa3432397bbdd62b761ac6197627bc2
Rafał Piotrkowski <rafalpiotrkowski@Rafas-MacBook-Pro.local>
2023-03-04 13:31:53 +0100

    created a project stucture

---
+++ lib/ui/home_page_wrapper/bloc/home_p_age_wrapper_event.dart
@@ -0,0 +1,8 @@
+part of 'home_p_age_wrapper_bloc.dart';
+
+abstract class HomePAgeWrapperEvent extends Equatable {
+  const HomePAgeWrapperEvent();
+
+  @override
+  List<Object> get props => [];
+}
